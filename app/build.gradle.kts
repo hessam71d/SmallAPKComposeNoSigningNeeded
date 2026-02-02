17a5738cbcf5646dafec593d42dc87080558dbed
Hessam Ahmadi <hessam71d@gmail.com>
2026-02-02 17:19:21 +0330

    updated libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
@@ -35,16 +34,13 @@ android {
                 "proguard-rules.pro"
             )
 
-            isDebuggable = false
             isShrinkResources = true
+            isDebuggable = false
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
